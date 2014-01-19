5547952f61f1a4df7906679a33c6eb45ff66b2a6
Simon Ser <contact@emersion.fr>
2014-01-19 18:14:04 +0100

    Fixed an issue with breadcrumb in LoginController.

--- ctrl/backend/login/LoginController.class.php
+++ ctrl/backend/login/LoginController.class.php
@@ -4,7 +4,12 @@ namespace ctrl\backend\login;
 class LoginController extends \core\BackController {
 	protected function _addBreadcrumb($page = array()) {
 		$breadcrumb = array(
-			array('url' => 'admin/module/'.$this->module, 'title' => 'Compte administrateur')
+			array(
+				'url' => $this->app->router()->getUrl('main', 'showModule', array(
+					'module' => $this->module()
+				)),
+				'title' => 'Compte administrateur'
+			)
 		);
 
 		$this->page()->addVar('breadcrumb', array_merge($breadcrumb, array($page)));
