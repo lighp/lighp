fd4cd11c434e6b1d3465b1f75d422e6dc1103ab7
Simon Ser <contact@emersion.fr>
2014-01-22 07:39:07 +0100

    Updated User.

--- core/User.class.php
+++ core/User.class.php
@@ -10,11 +10,35 @@ class User extends ApplicationComponent {
 		return (isset($_SESSION['user_admin']) && $_SESSION['user_admin'] === true);
 	}
 
+	public function isLogged() {
+		return (isset($_SESSION['user_logged']) && $_SESSION['user_logged'] === true);
+	}
+
+	public function permissions() {
+		return array();
+	}
+
+	public function can($permission) {
+		return false;
+	}
+
 	public function setAdmin($isAdmin) {
 		if (!is_bool($isAdmin)) {
 			throw new \InvalidArgumentException('Invalid user admin value');
 		}
 
 		$_SESSION['user_admin'] = $isAdmin;
+	}
+
+	public function setLogged($isLogged) {
+		if (!is_bool($isLogged)) {
+			throw new \InvalidArgumentException('Invalid user logged value');
+		}
+
+		$_SESSION['user_logged'] = $isLogged;
+	}
+
+	public function setPermissions(array $permissions) {
+		$_SESSION['user_permissions'] = $permissions;
 	}
 }
